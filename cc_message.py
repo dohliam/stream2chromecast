0ab4da9a03f7f6d8a28beec42734c1ba8d3a2bb5
Pat Carter <pat.128@gmail.com>
2016-08-08 19:38:57 +0100

    licence boilerplate

--- cc_message.py
+++ cc_message.py
@@ -9,6 +9,26 @@ Thanks to TheCrazyT for this very helpful gist : https://gist.github.com/TheCraz
 
 """
 
+
+# Copyright (C) 2014-2016 Pat Carter
+#
+# This file is part of Stream2chromecast.
+#
+# Stream2chromecast is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Stream2chromecast is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Stream2chromecast.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
 from struct import pack, unpack
 
 
